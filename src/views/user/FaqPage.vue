e17e78047819d49ce6a096c4241a1d6f7a3aa771
Alexandar Jevremcevic <aleksandr.jrem.dev@gmail.com>
2023-09-19 22:14:05 -0700

    Contact, Faq page updated

--- src/views/user/FaqPage.vue
+++ src/views/user/FaqPage.vue
@@ -167,16 +167,3 @@
     </div>
   </section>
 </template>
-<script>
-import { defineComponent } from "vue";
-import { GoogleMap, Marker } from "vue3-google-map";
-
-export default defineComponent({
-  components: { GoogleMap, Marker },
-  setup() {
-    const center = { lat: 40.689247, lng: -74.044502 };
-
-    return { center };
-  },
-});
-</script>
